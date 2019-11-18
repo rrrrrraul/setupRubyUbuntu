3dd59f046feb6619d2cdbbb32ad3e97280300828
Raul <rrrrrraul@users.noreply.github.com>
2019-11-17 22:34:22 -0800

    updated version of Ruby

--- setupRubyUbuntu.sh
+++ setupRubyUbuntu.sh
@@ -8,8 +8,8 @@ cd
 #curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 #echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-#sudo apt-get update
-#sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+sudo apt update
+sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
 # now install rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
@@ -21,8 +21,8 @@ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 exec $SHELL
 
-rbenv install 2.5.7
-rbenv global 2.5.7
+rbenv install 2.6.5
+rbenv global 2.6.5
 ruby -v
 
 # now install bundler
