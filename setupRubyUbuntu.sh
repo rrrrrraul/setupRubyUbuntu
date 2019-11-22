1c7d051ce1b55f954c5d7dd54d030568bbce29b8
Raul <rrrrrraul@users.noreply.github.com>
2019-11-21 22:02:01 -0800

    added back in node & yarn

--- setupRubyUbuntu.sh
+++ setupRubyUbuntu.sh
@@ -18,10 +18,10 @@
 # set up repo locations
 cd
 
-#sudo apt install curl
-#curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-#echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update
 sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
